d5e62b78f3d2d765d58ebd772455c4fe5515d69a
Nicocere <nm.cerecedo@outlook.com>
2022-11-18 13:21:55 -0300

    Update Form.js

--- src/componentes/Form/Form.js
+++ src/componentes/Form/Form.js
@@ -98,7 +98,8 @@ const Form = ({ itemSelected, cart, clearCart, handleId }) => {
             
             // debo ahora subir la pagina de backend a vercel y agregar el link aca
             url: "https://app-payments-envioflores.herokuapp.com/payment"
-            
+            // url: "http://localhost:3000/payment"
+            // url: "https://app-mercado-pago.vercel.app/"
         }
         console.log("OPTS", opts)
 
